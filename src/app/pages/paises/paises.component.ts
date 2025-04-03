fd4056e070ab50e55b287a9e5cc758ff6a56dfff
marco <marco@gmail.com>
2025-04-03 10:59:52 +0200

    hola

--- src/app/pages/paises/paises.component.ts
+++ src/app/pages/paises/paises.component.ts
@@ -1,24 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-paises',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './paises.component.html',
   styleUrl: './paises.component.css'
 })
 export class PaisesComponent {
   paisBuscar: string="";
   paisRecibido: any;  
+  continente: any;
+  error: any;
   getPaisesWithFetch(pais:string) {
-    fetch(`https://restcountries.com/v3.1/capital/${pais}`)
+    fetch(`https://restcountries.com/v3.1/name/${pais}?fullText=false`)
       .then(response => response.json())
       .then(data => {
-        this.paisRecibido = data[0]
-        console.log('Fetch:', data)
+      this.paisRecibido = data[0]
+      this.continente = data[0].continents[0]
+      this.error = null;
+      console.log('Fetch:', data)
       })
       .catch(error => {
+        this.error = error;
+        this.paisRecibido = null;
       console.error('Error con Fetch:', error)
   });
   }
